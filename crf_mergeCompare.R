9e20f8e477fac584600fd7be5ed8a07458dff8d4
Meghasyam <meghasyam@sagebase.org>
2018-09-19 13:24:32 -0700

    Add files via upload
    Added polar data to the merged data

--- crf_mergeCompare.R
+++ crf_mergeCompare.R
@@ -28,13 +28,13 @@ synapseLogin()
 
 ## Download both tables crf and fitbit
 
-crf.tableId = 'syn12010238'
-fitbit.tableId = 'syn12550816'
-name = 'Cardio 12MT-v5'
+# crf.tableId = 'syn12010238'
+# fitbit.tableId = 'syn12550816'
+# name = 'Cardio 12MT-v5'
 
-# crf.tableId = 'syn12010132'
-# fitbit.tableId = 'syn12550818'
-# name = 'Cardio Stair Step-v1'
+crf.tableId = 'syn12010132'
+fitbit.tableId = 'syn12550818'
+name = 'Cardio Stair Step-v1'
  
 # crf.tableId = 'syn12010237'
 # fitbit.tableId = 'syn12550817'
@@ -65,6 +65,35 @@ colN <- rownames(merged.tbl)
 merged.tbl <- merged.tbl %>% transpose() %>% `colnames<-`(colN)
 merged.tbl$fitbit.timestamp <- as.POSIXct(as.numeric(merged.tbl$fitbit.timestamp), origin = '1970-01-01')
 
+
+# Merge Polar data
+tableId = 'syn16809558'
+polar.tbl <- synGet(tableId)@filePath %>% 
+  read.csv() %>% 
+  dplyr::select(-X) %>% 
+  dplyr::select(hr, externalId, timestamp)
+
+polar.tbl$timestamp <- strptime(polar.tbl$timestamp, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+merged.tbl$startTime <- strptime(merged.tbl$startTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+merged.tbl$stopTime <- strptime(merged.tbl$stopTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+polar.tbl$externalId <- as.character(polar.tbl$externalId)
+
+merged.tbl.2 <- apply(merged.tbl,1,function(x){ 
+  tryCatch({
+    polar.data <- polar.tbl %>% dplyr::filter(externalId == x['externalId']) %>% 
+      dplyr::filter(timestamp <= x['stopTime']) %>%  dplyr::filter(timestamp >= x['startTime'])
+    x['polar.timestamp'] <- median(polar.data$timestamp)
+    x['polar.hr'] <- median(polar.data$hr)
+    return(x)
+  },
+  error = function(e){ NA })
+}) %>% as.data.frame() 
+
+colN <- rownames(merged.tbl.2)
+merged.tbl.2 <- merged.tbl.2 %>% transpose() %>% `colnames<-`(colN)
+merged.tbl.2$polar.timestamp <- as.POSIXct(as.numeric(merged.tbl.2$polar.timestamp), origin = '1970-01-01')
+
+
 #############
 # Upload to Synapse
 #############
@@ -77,7 +106,7 @@ thisRepo <- getRepo(repository = "itismeghasyam/CRF_validation_analysis", ref="b
 thisFile <- getPermlink(repository = thisRepo, repositoryPath=thisFileName)
 
 # Write to Synapse
-write.csv(merged.tbl,file = paste0('merged',name,'.csv'),na="")
+write.csv(merged.tbl.2,file = paste0('merged',name,'.csv'),na="")
 obj = File(paste0('merged',name,'.csv'), 
            name = paste0('merged',name,'.csv'), 
            parentId = 'syn11968320')

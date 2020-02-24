1e195fe43f08fafa13903d908adbf4f228c7ab17
itismeghasyam <rstudio@ip-10-5-26-136.ec2.internal>
2020-02-24 22:13:46 +0000

    - removed test code - refined code

--- analysis/crf_mergeCompare.R
+++ analysis/crf_mergeCompare.R
@@ -1,6 +1,6 @@
 ########################################################################
 # CRF Project 
-# Purpose: To merge fitbit data and crf app data to compare the results
+# Purpose: To merge fitbit, polar and crf app data to compare the results
 # Author: Meghasyam Tummalacherla
 # email: meghasyam@sagebase.org
 ########################################################################
@@ -14,139 +14,98 @@ library(data.table)
 library(tidyr)
 library(plyr)
 library(dplyr)
-library(seewave)
-library(mhealthtools) 
 library(synapser)
 library(githubr)
-library(ggplot2)
-library(parsedate)
 library(lubridate)
-library(CovariateAnalysis)
 
 synLogin()
 
 ## Download both tables crf and fitbit
+# SynIds and names of reference tables 
+ref.details <- data.frame(crf_tableId = c('syn12010238',
+                                      'syn12010132',
+                                      'syn12010237'),
+                          fitbit_tableId = c('syn12550816',
+                                          'syn12550818',
+                                          'syn12550817'),
+                          polar_tableId = c('syn16811362',
+                                          'syn16811501',
+                                          'syn16811363'),
+                          name = c('Cardio 12MT-v5',
+                                   'Cardio Stair Step-v1',
+                                   'Cardio Stress Test-v1'))
 
-crf.tableId = 'syn12010238'
-fitbit.tableId = 'syn12550816'
-polar.tableId = 'syn16811362'
-name = 'Cardio 12MT-v5'
-
-# crf.tableId = 'syn12010132'
-# fitbit.tableId = 'syn12550818'
-# polar.tableId = 'syn16811501'
-# name = 'Cardio Stair Step-v1'
+for(i in 1:nrow(ref.details)){
   
-# crf.tableId = 'syn12010237'
-# fitbit.tableId = 'syn12550817'
-# polar.tableId = 'syn16811363'
-# name = 'Cardio Stress Test-v1'
+  # SynIds of the required tables
+  crf.tableId = ref.details$crf_tableId[i]
+  fitbit.tableId = ref.details$fitbit_tableId[i]
+  polar.tableId = ref.details$polar_tableId[i]
+  name = ref.details$name[i]
+  
+  # Download the tables from synapse
+  crf.tbl <- read.csv(synapser::synGet(crf.tableId)$path) %>% dplyr::select(-X)
+  fitbit.tbl <- read.csv(synapser::synGet(fitbit.tableId)$path) %>% dplyr::select(-X)
+  polar.tbl <- read.csv(synapser::synGet(polar.tableId)$path) %>% dplyr::select(-X)
+  all.used.ids <- c(crf.tableId, fitbit.tableId, polar.tableId) # provenance tracking
+  
+  # Convert times into POSIXlt format
+  fitbit.tbl$timestamp <- strptime(fitbit.tbl$timestamp, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  crf.tbl$startTime <- strptime(crf.tbl$startTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  crf.tbl$stopTime <- strptime(crf.tbl$stopTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  polar.tbl$timestamp <- strptime(polar.tbl$timestamp, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  polar.tbl$externalId <- as.character(polar.tbl$externalId)
+  
+  # Merge crf and fitbit data
+  merged.tbl <- apply(crf.tbl,1,function(x){ 
+    tryCatch({
+      fitbit.data <- fitbit.tbl %>% dplyr::filter(healthCode == x['healthCode']) %>% 
+        dplyr::filter(timestamp <= x['stopTime']) %>%  dplyr::filter(timestamp >= x['startTime'])
+      
+      x['fitbit.timestamp'] <- median(fitbit.data$timestamp)
+      x['fitbit.hr'] <- median(fitbit.data$fitbitHR)
+      return(x)
+    },
+    error = function(e){ NA })
+  }) %>% as.data.frame()
+  
+  colN <- rownames(merged.tbl)
+  merged.tbl <- merged.tbl %>% transpose() %>% `colnames<-`(colN)
+  merged.tbl$fitbit.timestamp <- as.POSIXct(as.numeric(merged.tbl$fitbit.timestamp), origin = '1970-01-01')
+  merged.tbl$startTime <- strptime(merged.tbl$startTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  merged.tbl$stopTime <- strptime(merged.tbl$stopTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
+  
+  # Merge polar data
+  merged.tbl.2 <- apply(merged.tbl,1,function(x){ 
+    tryCatch({
+      polar.data <- polar.tbl %>% dplyr::filter(externalId == x['externalId']) %>% 
+        dplyr::filter(timestamp <= x['stopTime']) %>%  dplyr::filter(timestamp >= x['startTime'])
+      x['polar.timestamp'] <- median(polar.data$timestamp)
+      x['polar.hr'] <- median(polar.data$hr)
+      return(x)
+    },
+    error = function(e){ NA })
+  }) %>% as.data.frame() 
+  
+  colN <- rownames(merged.tbl.2)
+  merged.tbl.2 <- merged.tbl.2 %>% transpose() %>% `colnames<-`(colN)
+  merged.tbl.2$polar.timestamp <- as.POSIXct(as.numeric(merged.tbl.2$polar.timestamp), origin = '1970-01-01')
+  
+  #############
+  # Upload to Synapse
+  #############
+  # Github link
+  gtToken = 'github_token.txt';
+  githubr::setGithubToken(as.character(read.table(gtToken)$V1))
+  thisFileName <- 'analysis/crf_mergeCompare.R'
+  thisRepo <- getRepo(repository = "itismeghasyam/CRF_validation_analysis", ref="branch", refName='sagebio_master')
+  thisFile <- getPermlink(repository = thisRepo, repositoryPath=thisFileName)
+  
+  # Write to Synapse
+  write.csv(merged.tbl.2,file = paste0('merged',name,'.csv'),na="")
+  obj = File(paste0('merged',name,'.csv'), 
+             name = paste0('merged',name,'.csv'), 
+             parentId = 'syn11968320')
+  obj = synStore(obj,  used = all.used.ids, executed = thisFile)
+}
 
-crf.tbl <- read.csv(synapser::synGet(crf.tableId)$path) %>% dplyr::select(-X)
-fitbit.tbl <- read.csv(synapser::synGet(fitbit.tableId)$path) %>% dplyr::select(-X)
-polar.tbl <- read.csv(synapser::synGet(polar.tableId)$path) %>% dplyr::select(-X)
-all.used.ids <- c(crf.tableId, fitbit.tableId, polar.tableId)
-
-# Convert times into POSIXlt format
-fitbit.tbl$timestamp <- strptime(fitbit.tbl$timestamp, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-crf.tbl$startTime <- strptime(crf.tbl$startTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-crf.tbl$stopTime <- strptime(crf.tbl$stopTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-polar.tbl$timestamp <- strptime(polar.tbl$timestamp, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-polar.tbl$externalId <- as.character(polar.tbl$externalId)
-
-merged.tbl <- apply(crf.tbl,1,function(x){ 
-  tryCatch({
-    fitbit.data <- fitbit.tbl %>% dplyr::filter(healthCode == x['healthCode']) %>% 
-    dplyr::filter(timestamp <= x['stopTime']) %>%  dplyr::filter(timestamp >= x['startTime'])
-    
-    x['fitbit.timestamp'] <- median(fitbit.data$timestamp)
-    x['fitbit.hr'] <- median(fitbit.data$fitbitHR)
-    return(x)
-  },
-           error = function(e){ NA })
-}) %>% as.data.frame()
-
-colN <- rownames(merged.tbl)
-merged.tbl <- merged.tbl %>% transpose() %>% `colnames<-`(colN)
-merged.tbl$fitbit.timestamp <- as.POSIXct(as.numeric(merged.tbl$fitbit.timestamp), origin = '1970-01-01')
-merged.tbl$startTime <- strptime(merged.tbl$startTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-merged.tbl$stopTime <- strptime(merged.tbl$stopTime, format = '%Y-%m-%d %H:%M:%S',tz='') %>% as.POSIXct()
-
-# Merge Polar data
-merged.tbl.2 <- apply(merged.tbl,1,function(x){ 
-  tryCatch({
-    polar.data <- polar.tbl %>% dplyr::filter(externalId == x['externalId']) %>% 
-      dplyr::filter(timestamp <= x['stopTime']) %>%  dplyr::filter(timestamp >= x['startTime'])
-    x['polar.timestamp'] <- median(polar.data$timestamp)
-    x['polar.hr'] <- median(polar.data$hr)
-    return(x)
-  },
-  error = function(e){ NA })
-}) %>% as.data.frame() 
-
-colN <- rownames(merged.tbl.2)
-merged.tbl.2 <- merged.tbl.2 %>% transpose() %>% `colnames<-`(colN)
-merged.tbl.2$polar.timestamp <- as.POSIXct(as.numeric(merged.tbl.2$polar.timestamp), origin = '1970-01-01')
-
-###########
-# Test Zone
-###########
-# Number of users across assays (polar, fitbit, crf and common to all three)
-nUsers <- list()
-nUsers$polar <- length(unique(polar.tbl$externalId))
-nUsers$crf <- length(unique(crf.tbl$externalId))
-nUsers$fitbit <- length(unique(fitbit.tbl$healthCode))
-nUsers$common <- fitbit.tbl %>% dplyr::select(healthCode) %>% unique() %>% 
-  dplyr::inner_join(crf.tbl %>% dplyr::select(externalId, healthCode)) %>% unique() %>%  
-  dplyr::inner_join(polar.tbl %>% dplyr::select(externalId)) %>% unique()
-
-# User start and stop times for each CRF, polar and fitbit records across all healthCodes/externalIds
-crf.user.times <- crf.tbl %>%
-  dplyr::select(recordId, healthCode, appVersion, phoneInfo,
-                externalId, startTime, stopTime,tag) %>% 
-  dplyr::group_by(recordId) %>% 
-  dplyr::mutate(startTime.rec = min(startTime)) %>% 
-  dplyr::mutate(stopTime.rec = max(stopTime)) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(-startTime, -stopTime) %>% 
-  unique()
-
-fitbit.user.times <- fitbit.tbl %>% 
-  dplyr::select(healthCode, timestamp) %>% 
-  dplyr::group_by(healthCode) %>% 
-  dplyr::mutate(startTime.fitbit = min(timestamp)) %>% 
-  dplyr::mutate(stopTime.fitbit = max(timestamp)) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(-timestamp) %>% 
-  unique()
-
-polar.user.times <- polar.tbl %>% 
-  dplyr::select(externalId, timestamp) %>% 
-  dplyr::group_by(externalId) %>% 
-  dplyr::mutate(startTime.polar = min(timestamp)) %>% 
-  dplyr::mutate(stopTime.polar = max(timestamp)) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(-timestamp) %>% 
-  unique()
-
-all.user.times.common <- crf.user.times %>% 
-  dplyr::inner_join(fitbit.user.times) %>% 
-  dplyr::inner_join(polar.user.times)
-
-#############
-# Upload to Synapse
-#############
-
-# Github link
-gtToken = 'github_token.txt';
-githubr::setGithubToken(as.character(read.table(gtToken)$V1))
-thisFileName <- 'crf_mergeCompare.R'
-thisRepo <- getRepo(repository = "Sage-Bionetworks/CRF_validation_analysis", ref="branch", refName='master')
-thisFile <- getPermlink(repository = thisRepo, repositoryPath=thisFileName)
-
-# Write to Synapse
-write.csv(merged.tbl.2,file = paste0('merged',name,'.csv'),na="")
-obj = File(paste0('merged',name,'.csv'), 
-           name = paste0('merged',name,'.csv'), 
-           parentId = 'syn11968320')
-obj = synStore(obj,  used = all.used.ids, executed = thisFile)
